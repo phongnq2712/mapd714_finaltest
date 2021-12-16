ce2397f0003bb820a221ec03ecc0d2ff49675b16
phongnq <phongnq2712@gmail.com>
2021-12-15 19:43:12 -0500

    first commit

---
+++ QuocPhongNgo_MAPD714_Finaltest/QuocPhongNgo_MAPD714_Finaltest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuocPhongNgo_MAPD714_Finaltest
+//
+//  Created by Phong on 15/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
