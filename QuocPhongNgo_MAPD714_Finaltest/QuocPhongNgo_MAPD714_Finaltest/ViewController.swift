26661a39a5a63b63218ec0c05f6bbb2a0db74500
phongnq <phongnq2712@gmail.com>
2021-12-16 00:56:33 -0500

    create layout for first screen

--- QuocPhongNgo_MAPD714_Finaltest/QuocPhongNgo_MAPD714_Finaltest/ViewController.swift
+++ QuocPhongNgo_MAPD714_Finaltest/QuocPhongNgo_MAPD714_Finaltest/ViewController.swift
@@ -1,17 +1,36 @@
-//
-//  ViewController.swift
-//  QuocPhongNgo_MAPD714_Finaltest
-//
-//  Created by Phong on 15/12/2021.
-//
+/**
+ * Final Test
+ * File Name:    ViewController.swift
+ * Author:         Quoc Phong Ngo
+ * Student ID:   301148406
+ * Version:        1.0
+ * Date Modified:   December 16th, 2021
+ */
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tvHeight: UITextField!
+    var placeholderHeight : UILabel!
+    
+    @IBOutlet weak var switchMode: UISwitch!
+    @IBOutlet weak var tvWeight: UITextField!
+    var placeholderWeight : UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Placeholder height.
+        placeholderHeight = UILabel()
+        placeholderHeight.text = "m"
+        placeholderHeight.font = UIFont.italicSystemFont(ofSize: CGFloat((15.0)))
+        placeholderHeight.sizeToFit()
+        tvHeight.addSubview(placeholderHeight)
+        // Placeholder weight
+        placeholderWeight = UILabel()
+        placeholderWeight.text = "kg"
+        placeholderWeight.font = UIFont.italicSystemFont(ofSize: CGFloat((15.0)))
+        placeholderWeight.sizeToFit()
+        tvWeight.addSubview(placeholderWeight)
     }
 
 
